a777c685097971cd90f9e91432c45f0e0f4cba3b
abu-bakr-dev <abubakarse005@gmail.com>
2025-07-23 02:58:51 +0500

    Fix: Correct server_onupdate attribute for updated_at column in Timestamp mixin

--- app/models/mixin.py
+++ app/models/mixin.py
@@ -14,6 +14,6 @@ class Timestamp:
     updated_at = Column(
         TIMESTAMP(timezone=True),
         server_default=func.now(),
-        onupdate=func.now(),
+        server_onupdate=func.now(),
         nullable=False,
     )
